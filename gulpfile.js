6dc785233e736595c3c52ac6b2be3e77f2cd7b19
Eduardo de Moura Rodrigues <eduardom.rodrigues@gmail.com>
2019-02-25 20:31:40 -0300

    task

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,93 @@
-const {src, dest, parallel} = require('gulp');
+const {
+  src,
+  dest,
+  parallel,
+  series,
+  watch
+} = require('gulp');
 const less = require('gulp-less');
 const minifyCSS = require('gulp-csso');
 const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const clean = require('gulp-clean');
+const reload = browserSync.reload;
 
-function html() {
-  return src('app/*.html')
-    .pipe(dest('build'));
+function clearAllHtmls() {
+  return src('build/*.html')
+    .pipe(clean({
+      force: true
+    }));
 }
 
-function css(){
+function clearAllJs() {
+  return src('build/js/*')
+    .pipe(clean({
+      force: true
+    }));
+}
+
+function clearAllCss() {
+  return src('build/css/*.css')
+    .pipe(clean({
+      force: true
+    }));
+}
+
+function html() {
+  clearAllHtmls();
+
+  console.log('html');
+
+  return src('app/*.html')
+    .pipe(dest('build'))
+    .pipe(browserSync.stream());
+}
+
+function css() {
+  clearAllCss();
+
   return src('app/css')
     .pipe(less())
     .pipe(minifyCSS())
-    .pipe(dest('build'));
+    .pipe(dest('build'))
+    .pipe(browserSync.stream());
 }
 
-function js(){
-  return src('app/js/*.js', {sourcemaps: true})
+function js() {
+  clearAllJs();
+
+
+  return src('app/js/*.js', {
+      sourcemaps: true
+    })
     .pipe(concat('js/app.min.js'))
-    .pipe(dest('build', {sourcemaps: true}));
+    .pipe(dest('build', {
+      sourcemaps: true
+    }))
+    .pipe(browserSync.stream());
 
 }
+
+
+function server() {
+
+  browserSync.init({
+    server: "./build"
+  });
+
+  watch(['app/*'], function (cb) {
+    html();
+    css();
+    js();
+
+    reload;
+    cb();
+  })
+
+
+}
+
+
 
 exports.dist = parallel(html, css, js);
+exports.dev = series(html, css, js, server);
