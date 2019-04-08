bc2133cf173906b749c1296c63f89cea94336f44
Eduardo de Moura Rodrigues <eduardom.rodrigues@gmail.com>
2019-04-08 19:34:53 -0300

    ainda sem nenhum framework, preocupante... Porém já com logo e possível header

---
+++ app/ts/main.ts
@@ -0,0 +1,9 @@
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById('logo-id').style.width = '90px';
+    } else {
+        document.getElementById('logo-id').style.width = '130px';
+    }
+}
+
+window.onscroll = function () { scrollFunction() };
