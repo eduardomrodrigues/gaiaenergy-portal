d1c0a07d2d1774497a22e77b89c15316aaf70528
Eduardo de Moura Rodrigues <eduardom.rodrigues@gmail.com>
2019-04-22 22:04:42 -0300

    pegando as manhas de html/css

--- app/ts/main.ts
+++ app/ts/main.ts
@@ -1,6 +1,6 @@
 function scrollFunction() {
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById('logo-id').style.width = '90px';
+        document.getElementById('logo-id').style.width = '70px';
     } else {
         document.getElementById('logo-id').style.width = '130px';
     }
