21ead3b3f3d05fc1f4fe08925db674de07a8ddd5
Eduardo de Moura Rodrigues <eduardom.rodrigues@gmail.com>
2019-04-23 17:49:30 -0300

    task

--- gulpfile.js
+++ gulpfile.js
@@ -121,8 +121,4 @@ function server() {
 
 
 exports.dist = series(cleanAll, transformSass, html, css, js, imageMin, copySitemap, copyRobots, copyJsVendor, copyCssVendor, removeTmp);
-<<<<<<< HEAD
 exports.dev = series(cleanAll, transformSass, html, css, js, imageMin, removeTmp, copySitemap, copyRobots, copyJsVendor, copyCssVendor, server);
-=======
-exports.dev = series(cleanAll, transformSass, html, css, js, imageMin, removeTmp, copySitemap, copyRobots, copyJsVendor, copyCssVendor, server);
->>>>>>> parent of 35b4338... tentando entender
